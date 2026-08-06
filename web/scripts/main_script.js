916db5f533ffcae7b7ebcef01161c8de03a17096
Adam Richards <RICHARDSA1@churchlands.wa.edu.au>
2026-08-06 16:16:58 +0800

    Small updates.

--- web/scripts/main_script.js
+++ web/scripts/main_script.js
@@ -1,1 +1,13 @@
 alert("This page is in eary development\nFeatures may not work correctly or be broken alltogether.");
+fetch('../pages/navbar.html')
+    .then(response => {
+        if (!response.ok) throw new Error('Failed to load navbar html doc');
+        return response.text(); // Get content as text
+    })
+    .then(html => {
+        document.getElementById('navbardocument').innerHTML = html; // Insert content
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('navbardocument').innerHTML = '<p>Failed to load navbar. &#128577;</p>';
+    });
