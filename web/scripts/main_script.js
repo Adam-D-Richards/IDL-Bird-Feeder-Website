961004935cdb87d728c5c56da16de5bafa662609
Adam Richards <RICHARDSA1@churchlands.wa.edu.au>
2026-08-07 14:10:37 +0800

    Small updates, adding the navbar, and navbar fallback.

--- web/scripts/main_script.js
+++ web/scripts/main_script.js
@@ -1,3 +1,9 @@
+let navbar_fallback = `<h3 class="navbar_title">Churchland Bird Feeder</h3>
+<nav class="navbar_nav">
+    <a class="navbar_link" href="/">Home</a> | <a class="navbar_link" href="/about">About</a>
+</nav>
+<hr class="navbar_divider">`;
+
 alert("This page is in eary development\nFeatures may not work correctly or be broken alltogether.");
 fetch('../pages/navbar.html')
     .then(response => {
@@ -9,5 +15,5 @@ fetch('../pages/navbar.html')
     })
     .catch(error => {
         console.error('Error:', error);
-        document.getElementById('navbardocument').innerHTML = '<p>Failed to load navbar. &#128577;</p>';
+        document.getElementById('navbardocument').innerHTML = navbar_fallback;
     });
